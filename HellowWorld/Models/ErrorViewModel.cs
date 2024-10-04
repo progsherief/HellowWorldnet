966281d37878ed52e0e0d37eea33768b2dbd6b21
progsherief <progsherief@gmail.com>
2024-10-04 11:10:43 +0300

    Add prpj files

---
+++ HellowWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HellowWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
